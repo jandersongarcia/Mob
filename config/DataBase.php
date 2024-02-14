6a96c2b6f946506ad861db537f6a567deb9b75a2
Janderson Garcia <70637518+jandersongarcia@users.noreply.github.com>
2024-02-14 12:16:00 -0400

    database

--- config/DataBase.php
+++ config/DataBase.php
@@ -37,7 +37,7 @@ return [
         'driver' => 'mysql',
         'host' => 'localhost',
         'port' => '3306',
-        'database' => 'mobidb',
+        'database' => 'eldorado',
         'username' => 'root',
         'password' => '',
         'charset' => 'utf8mb4',
